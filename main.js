b3008142818ade4dffdaaac4b0f7f11f00b76cbd
jonheslop <jon.heslop@pusher.com>
2021-03-24 12:33:53 +0000

    Refactor to work with safari
    Needs to be within user triggered event

--- main.js
+++ main.js
@@ -4,13 +4,17 @@ const beamsClient = new PusherPushNotifications.Client({
   instanceId: "1c4b4b41-2d70-42eb-8108-c1dd8f51be22",
 });
 
-beamsClient
-  .start()
-  .then((beamsClient) => beamsClient.getDeviceId())
-  .then((deviceId) =>
-    console.log("Successfully registered with Beams. Device ID:", deviceId)
-  )
-  .then(() => beamsClient.addDeviceInterest("debug-hello"))
-  .then(() => beamsClient.getDeviceInterests())
-  .then((interests) => console.log("Current interests:", interests))
-  .catch(console.error);
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("subscribe").addEventListener("click", (event) => {
+    beamsClient
+      .start()
+      .then((beamsClient) => beamsClient.getDeviceId())
+      .then((deviceId) =>
+        console.log("Successfully registered with Beams. Device ID:", deviceId)
+      )
+      .then(() => beamsClient.addDeviceInterest("debug-hello"))
+      .then(() => beamsClient.getDeviceInterests())
+      .then((interests) => console.log("Current interests:", interests))
+      .catch(console.error);
+  });
+});
